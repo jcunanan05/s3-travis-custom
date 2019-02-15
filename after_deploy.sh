ae2cf2ba7453d969d55edceaf0aa1ba14b8dd269
Jonathan Cunanan <j_cunanan05@yahoo.com>
2019-02-14 19:06:45 -0500

    Put s3 redirect config into a folder

--- after_deploy.sh
+++ after_deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-aws s3api put-bucket-website --bucket $S3_BUCKET_NAME --website-configuration file://redirect-rules.json --region us-east-1
+aws s3api put-bucket-website --bucket $S3_BUCKET_NAME --website-configuration file://s3-config/redirect-rules.json --region us-east-1
 
 echo "deploy is finished"
